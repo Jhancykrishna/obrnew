fc61d586672b5e72b284092061520a7360d36488
Febin <febdan@gmail.com>
2021-11-05 16:40:25 +0530

    Download and upload

--- obr/src/main/java/com/sayone/obr/shared/PublisherUtils.java
+++ obr/src/main/java/com/sayone/obr/shared/PublisherUtils.java
@@ -5,25 +5,21 @@ import org.springframework.stereotype.Component;
 import java.security.SecureRandom;
 import java.util.Random;
 
-
 @Component
-public class Utils {
+public class PublisherUtils {
 
     private final Random RANDOM = new SecureRandom();
+    private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklnopqrstuvwxyz";
 
-    public String generateUserId(int length){
-        return generateRandomString(length);
+    public String generatePublisherId(int length) {
+        return  generateRandomString(length);
     }
 
     private String generateRandomString(int length) {
-        StringBuilder returnValue = new StringBuilder (length);
-
-        for (int i = 0; i < length; i++) {
-            String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder returnValue = new StringBuilder(length);
+        for (int i=0; i<length; i++) {
             returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
         }
         return new String(returnValue);
     }
-
-
 }
