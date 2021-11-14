8b9c4a0cf02df0f1b620c16032c4531a574d0151
Febin Koshy Daniel <90131485+fkdofficial@users.noreply.github.com>
2021-11-14 15:19:42 +0530

    Update BookRepository.java

--- obr/src/main/java/com/sayone/obr/repository/BookRepository.java
+++ obr/src/main/java/com/sayone/obr/repository/BookRepository.java
@@ -8,22 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
-
-  //for upload book
-  @Query(value = "SELECT * FROM book b where b.book_id=?1", nativeQuery = true)
-
-  Optional<BookEntity> findUploadArea(Long bookId) ;
-  //to delete
-  @Query(value = "SELECT * FROM book b where b.book_id=?1", nativeQuery = true)
-
-  BookEntity findById(Long bId, String userId);
-
-  //to delete book
-  @Query(value = "SELECT * FROM book b where b.book_id=?1", nativeQuery = true)
-  Optional<BookEntity> findByDeleteArea(Long bookId);
-
-
-  //BookEntity findById(long bookId);
+  BookEntity findById(long bookId);
 
   //to delete book
   @Query(value = "SELECT * FROM book b where b.book_id=?1", nativeQuery = true)
