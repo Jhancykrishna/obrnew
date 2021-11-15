fdf0ab14f0885fd97d2a1ec38bfb8b2ae20766d6
jhancykrishna <jhancykrishna94@gmail.com>
2021-11-15 20:32:28 +0530

    initial commit

--- obr/src/main/java/com/sayone/obr/service/UserService.java
+++ obr/src/main/java/com/sayone/obr/service/UserService.java
@@ -4,9 +4,20 @@ import com.sayone.obr.dto.UserDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
+
+   //user
     UserDto createUser(UserDto user);
 
     UserDto getUser(String email);
+
+    UserDto getUserByUserId(String userId);
+
+
+    UserDto updateUser( String userId,UserDto user);
+
+    void deleteUserById(String userId);
+
+  //publisher
 
     UserDto getPublisherById(String userId);
 
